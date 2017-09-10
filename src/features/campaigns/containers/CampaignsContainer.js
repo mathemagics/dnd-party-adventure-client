3f4cacc7a838d10f0ef7a0cb71731cc553c4b742
Paul Bodnar <pbodnar@rennie.com>
2017-09-09 22:04:40 -0700

    add campaigns navigation

--- src/features/campaigns/containers/CampaignsContainer.js
+++ src/features/campaigns/containers/CampaignsContainer.js
@@ -5,7 +5,10 @@ import { connect } from 'react-redux';
 import { Switch, Route, withRouter } from 'react-router-dom';
 
 import { fetchCampaigns } from 'raft/CampaignsDuck';
+
+import CampaignsNav from 'campaigns/components/campaignsNav';
 import CampaignsIndex from 'campaigns/components/campaignsIndex';
+import CampaignsCreate from 'campaigns/components/campaignsCreate';
 
 const mapStateToPorops = (state) => {
   const campaigns = state.getIn(['campaigns', 'campaignList']);
@@ -29,7 +32,11 @@ class CampaignsContainer extends PureComponent {
   render() {
     const { match } = this.props;
     return (
-      <Route exact path={match.url} render={this.renderIndex} />
+      <div>
+        <CampaignsNav match={match} />
+        <Route exact path={match.url} render={this.renderIndex} />
+        <Route path={`${match.url}/create`} component={CampaignsCreate} />
+      </div>
     );
   }
 }
